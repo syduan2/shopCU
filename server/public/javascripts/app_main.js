5664157caed21f1fc35769b7e447f8905e6eb995
Sunny Duan <syduan@nr0167.urh.illinois.edu>
2015-03-24 20:55:24 -0500

    yey

--- server/public/javascripts/app_main.js
+++ server/public/javascripts/app_main.js
@@ -5,14 +5,25 @@ app.controller("feed_populate",['$scope',function($scope){
   $scope.input_url="google.com";
   $scope.categories=['Tech','Clothes','Textbooks'];
   $scope.items=[
-    {'name' : 'camera', 'url' : 'http://ecx.images-amazon.com/images/I/319lZQWPEuL._AC_SY220_.jpg'},
-    {'name' : 'phone', 'url' : 'http://ecx.images-amazon.com/images/I/41K8jK0oS1L._AC_SY220_.jpg'},
-    {'name' : 'movie', 'url' : 'http://ecx.images-amazon.com/images/I/51Cfy5iCp7L._AC_SY220_.jpg'},
-    {'name' : 'ipod', 'url' : 'http://ecx.images-amazon.com/images/I/41cwEhtosRL._AC_SY220_.jpg'},
-    {'name' : 'camera', 'url' : 'http://ecx.images-amazon.com/images/I/319lZQWPEuL._AC_SY220_.jpg'},
+    {'Name' : 'camera', 'img_link' : 'http://ecx.images-amazon.com/images/I/319lZQWPEuL._AC_SY220_.jpg'},
+    {'Name' : 'phone', 'img_link' : 'http://ecx.images-amazon.com/images/I/41K8jK0oS1L._AC_SY220_.jpg'},
+    {'Name' : 'movie', 'img_link' : 'http://ecx.images-amazon.com/images/I/51Cfy5iCp7L._AC_SY220_.jpg'},
+    {'Name' : 'ipod', 'img_link' : 'http://ecx.images-amazon.com/images/I/41cwEhtosRL._AC_SY220_.jpg'},
+    {'Name' : 'camera', 'img_link' : 'http://ecx.images-amazon.com/images/I/319lZQWPEuL._AC_SY220_.jpg'},
     ];
+  $scope.init=function(){
+    $http.get('/items').
+    success(function(data, status, headers, config) {
+      $scope.items.concat(angular.fromJson(data));
+    }).
+    error(function(data, status, headers, config) {
+      console.log("OHNOES")
+    });
+  };
+
+
   $scope.add_item=function(){
-    $scope.items.push({'name' : $scope.input_name, 'url' : $scope.input_url});
+    $scope.items.push({'Name' : $scope.input_name, 'url' : $scope.input_url});
   };
   $scope.search=function(){
     $http.get('/search?q='+$scope.search_box);
