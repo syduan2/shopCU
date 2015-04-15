25d45294f71fd62fa33edc9e17896c57b47db0a5
Code-Omega <ltomzh@gmail.com>
2015-04-15 22:40:34 +0000

    added user

--- server/models/items.js
+++ server/models/items.js
@@ -20,4 +20,27 @@ module.exports=function(mongoose){
     data: Buffer
   });
   mongoose.model('Image', imgSchema);
+    
+  var userSchema = new mongoose.Schema({
+    username: {type:String,required: true,index: { unique: true }},
+    lastname: String,
+    firstname: String,
+    status: Number,
+    online: Boolean,
+    email: {type:String,required: true,index: { unique: true }},
+    password: { type: String, set: function(newValue) {return Hash.isHashed(newValue) ? newValue : Hash.generate(newValue);} },
+    phone: Number,
+    methods: {facebook: Boolean,
+              call: Boolean,
+              txtmsg: Boolean,
+              email: Boolean },
+    postedItems : [String],
+    image: String,
+    dateCreated : {type:Date,default:Date.now}
+  });
+  mongoose.model('User', userSchema);
+  mongoose.model('User', userSchema).on('index', function (err) {
+    if (err) console.error(err);
+  });
+    
 };
