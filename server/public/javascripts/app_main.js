2001e6ff98ec6e4bfe50124b4cded5766bc3a37b
Sunny Duan <syduan@nr0167.urh.illinois.edu>
2015-04-17 13:28:58 -0500

    yey

--- server/public/javascripts/app_main.js
+++ server/public/javascripts/app_main.js
@@ -1,4 +1,4 @@
-var app=angular.module("shopcu_main", ['ngRoute', 'controllers']);
+var app=angular.module("shopcu_main", ['ngRoute', 'controllers', 'ui.bootstrap']);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
