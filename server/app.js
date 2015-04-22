705cdf9f5bcb066892d1196b58125679e9bde1d8
Sunny Duan <syduan@nr0167.urh.illinois.edu>
2015-04-21 20:13:15 -0500

    lkdjf

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 
 
-var server = app.listen(80, '172.31.36.56', function () {
+//var server = app.listen(80, '172.31.36.56', function () {
 //var server = app.listen(80, 'localhost', function () {
   var host = server.address().address
   var port = server.address().port
